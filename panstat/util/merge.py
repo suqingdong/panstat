2736ab0e049a1a21e7fae00e1cb9eb9c4f8f7b64
suqingdong <1078595229@qq.com>
2023-11-09 13:33:24 +0800

    use multiprocessing to merge

--- panstat/util/merge.py
+++ panstat/util/merge.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from functools import partial
 from multiprocess import Pool
 
 import numpy as np
@@ -6,28 +7,36 @@ import numpy as np
 from . import logger
 
 
+def merge_path_result(merge_dir: str, path: Path):
+
+    logger.debug(f'merge result for: {path.name}')
+
+    out_path = Path(merge_dir) / path.name / f'{path.name}.txt'
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+
+    sum_data = None
+    for file in path.glob('*.txt'):
+        logger.debug(f'read file: {file.name}')
+        data = np.loadtxt(file, dtype=np.int64)
+        if sum_data is None:
+            sum_data = data
+        else:
+            sum_data += data
+
+    np.savetxt(out_path, sum_data, fmt='%d')
+
+    logger.debug(f'saved merged results to {out_path}')
+
+
+
+
 def merge_result(result_dir: str, merge_dir: str = 'merge'):
     result_dir = Path(result_dir)
 
     logger.debug(f'stat from result dir: {result_dir}')
 
-    for p in (result_dir.glob('[xy]*')):
-        logger.debug(f'merge result for: {p.name}')
-
-        out_path = Path(merge_dir) / p.name / f'{p.name}.txt'
-        out_path.parent.mkdir(parents=True, exist_ok=True)
-
-        sum_data = None
-        for file in p.glob('*.txt'):
-            logger.debug(f'read file: {file.name}')
-            data = np.loadtxt(file, dtype=np.int64)
-            if sum_data is None:
-                sum_data = data
-            else:
-                sum_data += data
-
-        with out_path.open('w') as out:
-            for line in sum_data:
-                out.write(f'{line}\n')
-
-        logger.debug(f'saved merged results to {out_path}')
+    result_paths = list(result_dir.glob('[xy]*'))
+    logger.debug(f'found {len(result_paths)} result paths: {result_paths}')
+    
+    with Pool() as pool:
+        pool.map(partial(merge_path_result, merge_dir), result_paths)
